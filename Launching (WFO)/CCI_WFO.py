e843e5c81a85981f049b97de1f20d3f8f35f4a05
Koustav Das <107167244+algoSiliguri@users.noreply.github.com>
2024-07-29 00:50:59 +0100

    Test WFO full flow

--- Launching (WFO)/CCI_WFO.py
+++ Launching (WFO)/CCI_WFO.py
@@ -7,34 +7,34 @@ warnings.filterwarnings("ignore")
 
 # Load the dataset
 df = pd.read_csv(
-  "../Upstox_Data/Create_Database/Nifty50_data/Daily/ITC_Daily.csv",
-  index_col="time",
-  parse_dates=True,
+    "../Upstox_Data/Create_Database/Nifty50_data/Daily/ADANIENT_Daily.csv",
+    index_col="time",
+    parse_dates=True,
 )
 
 # Define the range of values for each parameter
 params_range = {
-  "cci_period": range(14, 20),  # Example range from 14 to 20
-  "atr_period": range(10, 16),  # Example range from 10 to 16
+    "cci_period": range(14, 16),  # Example range from 14 to 20
+    # "atr_period": range(10, 16),  # Example range from 10 to 16
 }
 
 # Define the fixed value for the cost parameter
-params_fixed = {"cost": 10, "atr_multiplier": 1.5}
+params_fixed = {"cost": 10, "atr_multiplier": 1.5, "atr_period": 15}
 
 # Calculate the length of the training set as 35% of the total dataset length
 length_train_set = int(len(df) * 0.35)
 
 # Initialize the WalkForwardOptimization class
 WFO = WalkForwardOptimization(
-  data=df,
-  TradingStrategy=CciStrategy,
-  fixed_parameters=params_fixed,
-  parameters_range=params_range,
-  length_train_set=length_train_set,  # Training set size as 70% of the dataset
-  pct_train_set=0.70,                 # 70% of the data for training
-  anchored=True,                      # Use anchored training sets
-  title="Walk-Forward Optimization",
-  randomness=1.00                     # Test all parameter combinations
+    data=df,
+    TradingStrategy=CciStrategy,
+    fixed_parameters=params_fixed,
+    parameters_range=params_range,
+    length_train_set=length_train_set,  # Training set size as 70% of the dataset
+    pct_train_set=0.70,  # 70% of the data for training
+    anchored=True,  # Use anchored training sets
+    title="Walk-Forward Optimization",
+    randomness=1.00,  # Test all parameter combinations
 )
 
 # Run the optimization
