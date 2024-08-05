9f763a4e24ffeff77acc2d9e7a0c50ccf2379a4d
Agniva Dutta <agnivacosmics@gmail.com>
2024-08-05 23:27:24 +0100

    WIP - initial commit for trading bot

--- Quantreo/CCI_Flag.py
+++ Quantreo/CCI_Flag.py
@@ -6,12 +6,14 @@ import DataPreprocessing as dp
 
 
 class CCI_Flag:
-    def __init__(self, input_dir, output_file, cci_window, atr_window, constant=0.015):
+    def __init__(self, input_dir, output_file, cci_window, atr_window, constant=0.015, atr_multiplier=1.5, take_profit=0.01,):
         self.input_dir = input_dir
         self.output_file = output_file
         self.cci_window = cci_window
         self.atr_window = atr_window
         self.constant = constant
+        self.atr_multiplier = atr_multiplier
+        self.take_profit = take_profit
 
     def process_csv_files(self):
         for file_name in os.listdir(self.input_dir):
@@ -38,19 +40,25 @@ class CCI_Flag:
                 # Logic to flag stocks for buying
                 cci_current = df[f"CCI_{self.cci_window}"].iloc[-1]
                 cci_previous = df[f"CCI_{self.cci_window}"].iloc[-2]
+                last_day_close = df['close'].iloc[-1]
+
+                # Update the trailing stop
+                current_atr = df[f"ATR_{self.atr_window}"].iloc[-1]
+                trailing_stop = last_day_close - self.atr_multiplier * current_atr
 
                 # Check if CCI was below -100 and then crosses above -100
                 if cci_previous < -100 < cci_current:
                     stock_name = file_name.replace('_day.csv', '')
-                    flagged_stocks.append(stock_name)
+                    flagged_stocks.append((stock_name, last_day_close, self.take_profit, trailing_stop))
 
         # After collecting flagged stocks
         if flagged_stocks:
-            # Create a DataFrame from the list of flagged stocks
-            result_df = pd.DataFrame(flagged_stocks)
+            # Create a DataFrame from the list of flagged stocks with column headers
+            result_df = pd.DataFrame(flagged_stocks,
+                                     columns=['Stock Name', 'Last Day Close', 'Take Profit', 'Trailing Stop'])
 
-            # Write the DataFrame to the output CSV file without the header
-            result_df.to_csv(self.output_file, index=False, header=False)
+            # Write the DataFrame to the output CSV file with the header
+            result_df.to_csv(self.output_file, index=False)
 
 
 # Example usage:
